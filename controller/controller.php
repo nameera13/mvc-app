1f9a019ab194faab4c93036f4750971791711258
Nameera Pathan <109778289+nameera13@users.noreply.github.com>
2022-07-24 14:57:48 +0530

    new added succesfully

--- controller/controller.php
+++ controller/controller.php
@@ -1,45 +1,51 @@
-<?php 
-require_once("model/model.php"); 
-class controller extends model 
-{
-    public function __construct() 
+<?php
+
+    require_once("model/model.php");
+
+    class controller extends model
     {
-        parent:: __construct();
-        // $name="hi krupanshi";
-        // echo $name;
-        // load your view here
-        if(isset($_SERVER["PATH_INFO"]))
+        public function __construct()
         {
-            switch($_SERVER["PATH_INFO"])
+            parent:: __construct();
+            
+            // echo "hello";
+            
+            $st=$this->selectalldata('state');
+
+            $ct=$this->selectalldata('city');
+
+            if(isset($_SERVER["PATH_INFO"]))
             {
-                case '/':
-                    require_once("index.php");
-                    require_once("header.php");
-                    require_once("navbar.php");
-                    require_once("content.php");
-                    require_once("footer.php");
-                    break;
-                case '/Contact-us':
-                    require_once("index.php");
-                    require_once("header.php");
-                    require_once("navbar.php");
-                    require_once("contact.php");
-                    require_once("footer.php");
-                    break;
-                default:
-                require_once("index.php");
-                require_once("header.php");
-                require_once("404.php");
-                break;
-                
+                switch($_SERVER["PATH_INFO"])
+                {
+                    case '/':
+                        require_once("index.php");
+                        require_once("header.php");
+                        require_once("navbar.php");
+                        require_once("content.php");
+                        require_once("footer.php");
+                        require_once("register.php"); 
+                        break;
+
+                        case '/Contact-us':
+
+                        require_once("index.php");
+                        require_once("header.php");
+                        require_once("navbar.php");
+                        require_once("contact.php");
+                        require_once("footer.php");
+                        break;
+
+                    default:
+                        require_once("index.php");
+                        require_once("header.php");
+                        require_once("404.php");
+                        break;
+                }
             }
-
         }
-
     }
-     
-}
 
-$obj=new  controller;
+    $obj = new controller;
 
 ?>
